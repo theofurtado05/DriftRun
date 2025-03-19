858909ff0648c0f72367ae9862595d6e09c85473
Theo Furtado Mauricio <58632127+theofurtado05@users.noreply.github.com>
2025-03-19 16:48:49 -0300

    fix: car velocity and dificult

--- js/config.js
+++ js/config.js
@@ -4,10 +4,10 @@ const CONFIG = {
         width: 2,
         height: 1,
         length: 4,
-        speed: 20, // Velocidade base
+        speed: 12, // Velocidade base
         maxSpeed: 50,
-        lateralSpeed: 5, // Aumentado para melhor resposta
-        rotationSpeed: 0.05, // Aumentado para curvas mais responsivas
+        lateralSpeed: 3, // Aumentado para melhor resposta
+        rotationSpeed: 0.02, // Aumentado para curvas mais responsivas
         driftRecoveryRate: 0.9, // Ajustado para drift mais persistente
         maxDriftForce: 0.5 // Aumentado para efeito visual mais forte
     },
@@ -27,7 +27,7 @@ const CONFIG = {
     game: {
         coinInterval: 2, // segundos entre grupos de moedas
         coinValue: 1,    // valor de cada moeda
-        difficultyIncrease: 0.01, // Aumento base de velocidade por frame
+        difficultyIncrease: 0.0001, // Aumento base de velocidade por frame
         initialObstacleInterval: 2.5,  // segundos
         maxObstacleInterval: 0.8,    // segundos
         maxDifficultyLevel: 10
